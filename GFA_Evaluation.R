83b0c9f92b22c12e35467d2c09b1f4dfc8586bed
Gewerd-Strauss <80548781+Gewerd-Strauss@users.noreply.github.com>
2023-09-06 19:09:19 +0200

    R: bugfix: minor breaks could get janky.

--- GFA_Evaluation.R
+++ GFA_Evaluation.R
@@ -1485,7 +1485,8 @@ RunDetailed <- function(ChosenDays,Files,PotsPerGroup,numberofGroups,groups_as_o
                 Limits[[2]] <- Limits[[2]] + breaks$BreakStepSize
                 breaks$breaknumber <- breaks$breaknumber + 1
             }
-            GFA_plot_box <- GFA_plot_box + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
+            mb <- seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize/2) ## generate minor breaks always inbetween major breaks.
+            GFA_plot_box <- GFA_plot_box + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = mb,n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
                                                               limits = c(Limits[[1]],Limits[[2]]))
             
             if (hasName(ini$Fontsizes,"Fontsize_PValue")) {
@@ -1885,7 +1886,8 @@ RunDetailed <- function(ChosenDays,Files,PotsPerGroup,numberofGroups,groups_as_o
                 Limits[[2]] <- Limits[[2]] + breaks$BreakStepSize
                 breaks$breaknumber <- breaks$breaknumber + 1
             }
-            GFA_plot_box <- GFA_plot_box + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
+            mb <- seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize/2) ## generate minor breaks always inbetween major breaks.
+            GFA_plot_box <- GFA_plot_box + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = mb,n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
                                                               limits = c(Limits[[1]],Limits[[2]]))
             
             stat.test$p.scient <- formatPValue(stat.test$p)
@@ -2464,7 +2466,8 @@ GFA_main <- function(folder_path,returnDays=FALSE,saveFigures=FALSE,saveExcel=FA
         Limits[[2]] <- Limits[[2]] + breaks$BreakStepSize
         breaks$breaknumber <- breaks$breaknumber + 1
     }
-    GFA_SummaryPlot <- GFA_SummaryPlot + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
+    mb <- seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize/2) ## generate minor breaks always inbetween major breaks.
+    GFA_SummaryPlot <- GFA_SummaryPlot + scale_y_continuous(breaks = seq(Limits[[1]],Limits[[2]],breaks$BreakStepSize),minor_breaks = mb,n.breaks = breaks$breaknumber, ## round_any is used to get the closest multiple of 25 above the maximum value of the entire dataset to generate tick
                                                             limits = c(Limits[[1]],Limits[[2]]))
     
     
